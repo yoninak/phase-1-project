cd251c9b9880a33b68719a187bab673e23e4dc92
Yoninia Karmely <yoninaweinberg28@gmail.com>
2023-09-03 21:27:35 -0400

    added style

--- index.js
+++ index.js
@@ -15,15 +15,7 @@ function getInfo(){
     //i succesfully got my data in the console
     // data.forEach(Object => {
       
-      // const feedingInfo = document.createElement("p")
-      // feedingInfo.textContent= "ounces per feed:" + Object.ouncesPerFeed 
-      // feedDiv.appendChild(feedingInfo)
-      // console.log(feedingInfo)
-      // //console.log(Element)
-      // const moreInfo = document.createElement("p") 
-      // console.log(moreInfo)
-      // moreInfo.textContent = "feeds per day:" + Object.feedsPerDay
-      // feedDiv.appendChild(moreInfo)
+      
     // });
     //"babyFeedByMonth" = data
     // "id": 1,
@@ -38,7 +30,45 @@ function getInfo(){
 
 function handleData(data){
   const selection = document.getElementById("months")
-  selection.addEventListener("click", () => {
-    console.log("event")
+  selection.addEventListener("change", (event) => {
+    iteration(selection.value, data)
+      
   })
+ 
+
 }
+
+
+function iteration(value, data){
+    data.forEach(month => {
+        if (value == month.id){
+
+            display(month)
+
+        }
+    });
+
+}
+
+function display(month){
+    feedDiv.removeChild(feedDiv.lastChild)
+    feedDiv.removeChild(feedDiv.lastChild)
+    const feedingInfo = document.createElement("p")
+    feedingInfo.textContent= "ounces per feed:" + month.ouncesPerFeed 
+    feedDiv.appendChild(feedingInfo)
+    const moreInfo = document.createElement("p") 
+    moreInfo.textContent = "feeds per day:" + month.feedsPerDay
+    feedDiv.appendChild(moreInfo)
+}
+ 
+ document.getElementById("header").addEventListener("click", (event) =>{
+  if (event.target.style.color  === "rgb(242, 48, 197)"){
+    return event.target.style.color  = 'blue'
+  }
+  else{
+    return event.target.style.color = "rgb(242, 48, 197)"
+  
+  }
+  
+ })
+
